fb1c9e156ddee88f2a0981796f98036fcd145fe9
Indyjen77 <jp46227@aol.com>
2024-03-01 16:33:44 -0500

    Additional changes from djangogirls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+path('drafts/', views.post_draft_list, name='post_draft_list'),
+path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
